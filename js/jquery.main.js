389613e6eca4b4eac43feec4c91fa9a508e47e9b
AsDmitrij <asdmitrij@mail.ru>
2016-12-22 19:06:48 +0300

    Add files via upload

---
+++ js/jquery.main.js
@@ -0,0 +1,141 @@
+$(function(){
+    //original field values
+    var field_values = {
+            //id        :  value
+            'username'  : 'Логин',
+            'password'  : 'Пароль',
+            'cpassword' : 'Пароль',
+            'firstname'  : 'Имя',
+            'lastname'  : 'Фамилия',
+            'email'  : 'email'
+    };
+
+
+    //inputfocus
+    $('input#firstname').inputfocus({ value: field_values['firstname'] });
+    $('input#lastname').inputfocus({ value: field_values['lastname'] });
+    $('input#country').inputfocus({ value: field_values['country'] }); 
+    $('input#city').inputfocus({ value: field_values['city'] });
+    $('input#index').inputfocus({ value: field_values['index'] });
+    $('input#street').inputfocus({ value: field_values['street'] }); 
+    $('input#house').inputfocus({ value: field_values['house'] }); 
+    $('input#flat').inputfocus({ value: field_values['flat'] }); 
+    $('input#mobile').inputfocus({ value: field_values['mobile'] }); 
+
+
+
+
+    //reset progress bar
+    $('#progress').css('width','0');
+    $('#progress_text').html('0% Выполнено');
+
+    //first_step
+    $('form').submit(function(){ return false; });
+    $('#submit_first').click(function(){
+        //remove classes
+        $('#first_step input').removeClass('error').removeClass('valid');
+
+        //ckeck if inputs aren't empty
+        var fields = $('#first_step input[type=text], #first_step input[type=password]');
+        var error = 0;
+        fields.each(function(){
+            var value = $(this).val();
+            if( value.length<4 || value==field_values[$(this).attr('id')] ) {
+                $(this).addClass('error');
+                $(this).effect("shake", { times:3 }, 50);
+                
+                error++;
+            } else {
+                $(this).addClass('valid');
+            }
+        });        
+        
+        if(!error) {
+            if( $('#password').val() != $('#cpassword').val() ) {
+                    $('#first_step input[type=password]').each(function(){
+                        $(this).removeClass('valid').addClass('error');
+                        $(this).effect("shake", { times:3 }, 50);
+                    });
+                    
+                    return false;
+            } else {   
+                //update progress bar
+                $('#progress_text').html('33% Выполнено');
+                $('#progress').css('width','113px');
+                
+                //slide steps
+                $('#first_step').slideUp();
+                $('#second_step').slideDown();     
+            }               
+        } else return false;
+    });
+
+
+    $('#submit_second').click(function(){
+        //remove classes
+        $('#second_step input').removeClass('error').removeClass('valid');
+
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+        var fields = $('#second_step input[type=text]');
+        var error = 0;
+        fields.each(function(){
+            var value = $(this).val();
+            if( value.length<1 || value==field_values[$(this).attr('id')] || ( $(this).attr('id')=='email' && !emailPattern.test(value) ) ) {
+                $(this).addClass('error');
+                $(this).effect("shake", { times:3 }, 50);
+                
+                error++;
+            } else {
+                $(this).addClass('valid');
+            }
+        });
+
+        if(!error) {
+                //update progress bar
+                $('#progress_text').html('66% Выполнено');
+                $('#progress').css('width','226px');
+                
+                //slide steps
+                $('#second_step').slideUp();
+                $('#third_step').slideDown();     
+        } else return false;
+
+    });
+
+
+    $('#submit_third').click(function(){
+        //update progress bar
+        $('#progress_text').html('100% Выполнено');
+        $('#progress').css('width','339px');
+
+        //prepare the fourth step
+        var fields = new Array(
+            $('#firstname').val(),
+            $('#lastname').val(),
+            $('#country').val(),
+            $('#firstname').val() ,
+            $('#city').val(),
+            $('#index').val(),
+            $('#street').val(),
+            $('#house').val(),
+            $('#flat').val(),
+            $('#mobile').val()                       
+        );
+        var tr = $('#fourth_step tr');
+        tr.each(function(){
+            //alert( fields[$(this).index()] )
+            $(this).children('td:nth-child(2)').html(fields[$(this).index()]);
+        });
+                
+        //slide steps
+        $('#third_step').slideUp();
+        $('#fourth_step').slideDown();            
+    });
+
+
+    $('#submit_fourth').click(function(){
+        //send information to server
+        alert('Данные отправлены');
+    });
+
+});
